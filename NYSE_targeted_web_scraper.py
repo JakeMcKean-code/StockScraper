b1213ab72ee70ac9d0480602e05b5424ab410590
Jake McKean <jake.mckean12@gmail.com>
2021-09-10 18:51:30 +0100

    New Scraper for NYSE site

--- NYSE_targeted_web_scraper.py
+++ NYSE_targeted_web_scraper.py
@@ -5,68 +5,6 @@ import numpy as np
 import time
 import datetime
 
-'''
-def web_content_div(web_content, class_path):
-    web_content_div = web_content.find_all('div',{'class': class_path})   # find specific div using a class path
-    try:
-        spans = web_content_div[0].find_all('span')   # inside each div there are spans, find them all
-        texts = [span.get_text() for span in spans]
-    except IndexError:
-        texts = [] # set to empty list if there is an index error for the spans in the divs
-    return texts
-
-# get functions to get the stock price from url
-def real_time_stock(stock_code):
-    url = 'https://ih.advfn.com/stock-market/NYSE/alcoa-' + stock_code + 'AA/stock-price' # generalise url so we can get any stock code
-    try:
-        r = requests.get(url)
-        web_content = BeautifulSoup(r.text,'html.parser') # get request and parse it
-        # we want to find the divs and then find the info inside the divs
-        texts = web_content_div(web_content, 'price-info') # got class path from the website
-        if texts != []:
-              # if it's not empty then we set the price and the change accordingly
-             price, change = texts[0], texts[1]
-        else: 
-              price, change = [], []
-        # Do the same but now searching different class paths for diffrerent information on the site
-        texts = web_content_div(web_content, 'TableElement')
-        if texts!=[]:
-            for count, vol in enumerate((texts)):
-                if vol == "Volume":
-                    print('here now for volsss --------------------')
-                    volume = texts[count + 1]
-                else:
-                    volume = []
-                    print('bad vols :(')
-
-    except ConnectionError:
-        price, change, volume, stock_code = [], [], [], [], [], []
-    return price, change, volume, stock_code # now we have a lot more information about any stock
-
-stocks = ['AA']
-
-while(True):
-    for stock_codes in stocks:
-        col = [] # column
-        timestamp = datetime.datetime.now() - datetime.timedelta(hours = 5) # take into account time difference between here and NY
-        timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S') # give specific format to the date and time
-        info = []
-        col = [timestamp] # make a column with the timestamp
-        stock_price, change, volume, stock_code = real_time_stock(stock_codes)
-        info.append(stock_code) # add the stock code to the list
-        info.extend([stock_price]) # extend the stock price and other info to make a 1D list of info
-        info.extend([change])
-        info.extend([volume])
-        col.extend(info) # add the timestamp to the start of the row
-        df = pd.DataFrame(col)
-        df= df.T # transpose it so it is sideways (along the x)
-        df.to_csv(str(timestamp[0:11]) + 'stock_data.csv', mode = 'a', header = False) # save to a csv file
-        info.clear()
-        col.clear()
-        del(df)
-    time.sleep(5)
-'''
-
 def get_data(code, company_name: list, company_ticker, company_stock, change, percent_change, volume):
     url: str = 'https://www.advfn.com/nyse/newyorkstockexchange.asp'
     page = requests.get(url)
