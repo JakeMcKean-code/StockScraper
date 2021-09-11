aca4a00c720f1e7416871df71785d79d14ebf610
Nathan <nathan.davies02@outlook.com>
2021-09-11 11:31:02 +0100

    Scraping method aligned with main branch

--- NYSE_targeted_web_scraper.py
+++ NYSE_targeted_web_scraper.py
@@ -1,3 +1,4 @@
+from os import name
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -5,7 +6,57 @@ import numpy as np
 import time
 import datetime
 
-def get_data(code, company_name: list, company_ticker, company_stock, change, percent_change, volume, time_stamp):
+
+def soup_parser():
+    url: str = 'https://www.advfn.com/nyse/newyorkstockexchange.asp'
+    page = requests.get(url)
+    page_text: str = page.text
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return soup
+
+def access_page_info():
+    soup = soup_parser()
+    info_rows: list = soup.find_all('tr', attrs={'class':['ts0', 'ts1']})
+    return info_rows
+
+def get_data(page_info, stock_code):
+    for element in page_info:
+        row = element.find_all('td')
+        if(row[1].text.strip() == stock_code):
+            company_name = row[0].text.strip()
+            company_ticker = row[1].text.strip()
+            company_stock = float(str(row[3].text.strip()))
+            change = float(str(row[4].text.strip()))
+            percent_change = float(str(row[5].text.strip()).replace('%',''))
+            volume = float(str(row[6].text.strip()).replace(',',''))
+
+            return  company_name, company_ticker, company_stock, change, percent_change, volume
+
+
+def scraping(stock_list, interupt_time, interval_time):#interupt and interval time are passed as seconds
+    df = pd.DataFrame(columns=["Time", "Company name", "Company ticker", "Stock Price", "Change", "Percent Change", "Volume"])
+
+    start_time = time.time()
+    current_time = time.time()
+
+    while current_time - start_time < interupt_time:
+        page_info = access_page_info()#returns the page information
+        time_stamp = datetime.datetime.now() - datetime.timedelta(hours=5)#creates a time stamp for when the page was accessed and converts UK time to NY time
+        time_stamp = time_stamp.strftime('%y-%m-%d %H:%M:%S')#format the time stamp
+        for stock_code in stock_list:
+            information: list = [time_stamp]#information list created and time_stamp added into it
+            information.extend(get_data(page_info, stock_code))#returns the stock information and adds it into the list of information
+            df.loc[len(df)] = information#adds the information list to the datatframe
+            df.to_csv('Test.csv',index=False)#saves all vales at once at the end of scraping
+
+        time.sleep(interval_time)#adds a sleep to the scaper after scraping the desired companies
+        current_time = time.time()
+
+stocks = ['AA', 'ABEV']
+scraping(stocks, 60, 5)
+
+
+'''def get_data(code, company_name: list, company_ticker, company_stock, change, percent_change, volume, time_stamp):
     url: str = 'https://www.advfn.com/nyse/newyorkstockexchange.asp'
     page = requests.get(url)
     page_text: str = page.text
@@ -29,8 +80,10 @@ def get_data(code, company_name: list, company_ticker, company_stock, change, pe
     change = np.array(change)
     percent_change = np.array(percent_change)
     volume = np.array(volume)
-  
+    
+    print(name)
     name = np.delete(name,0)
+    print(name)
     ticker = np.delete(ticker,0)
     stock = np.delete(stock,0)
     
@@ -44,7 +97,7 @@ def get_data(code, company_name: list, company_ticker, company_stock, change, pe
 
     merged = np.concatenate((time, name,ticker,stock, change, percent_change, volume),axis=1)
     data = pd.DataFrame({"Time": merged[:,0], "Company name":merged[:,1], "Company ticker":merged[:,2],"Stock Price":merged[:,3], "Change":merged[:,4], "Percent Change":merged[:,5], "Volume":merged[:,6]})
-    print(data.head())
+    #print(data.head())
     return data
 
 stocks = ['AA', 'ABEV']
@@ -59,7 +112,7 @@ while True:
         percentage_change = []
         volume = []
         df = get_data(code, company_name, company_ticker, company_stock, change, percentage_change, volume, timestamp)
-        df.to_csv(str(timestamp[0:11]) + 'stock_data.csv', mode = 'a', header = False, index=None)
+        #df.to_csv(str(timestamp[0:11]) + 'stock_data.csv', mode = 'a', header = False, index=None)
         del(df)
         print('----------------')
-    time.sleep(5)
+    time.sleep(5)'''
